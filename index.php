885c39281af7aa2ad0174b91e35de68886bb08c0
Salah-Kafaji <salah94master@gmail.com>
2023-11-18 20:39:11 +0300

    edite image and title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-    <title>شعبة التخطيط والتطوير - العتبة العلوية المقدسة</title>
+    <title>مركز المحسن لثقافة الاطفال</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style_sheet/index.css">
 </head>
@@ -19,7 +19,7 @@
         </header>
 
         <div class="SH_main_container">
-            <h1>شعبة التخطيط والتطوير</h1>
+            <h1>مركز المحسن لثقافة الاطفال</h1>
             <ul class="SH_links_list">
                 <li>
                     <a href="https://youtube.com/@qanbartv?si=BgcchSuDNjIR9Cw6" target="_blank">
@@ -61,7 +61,7 @@
                         <img class="SH-top-img" src="image/iahs-green.png" >
                         <h3>مركز المحسن لثقافة الاطفال</h3>
                         <div class="SH_image_container">
-                            <img class="SH_image" src="image/BG/re-telegram.png" alt="">
+                            <img class="SH_image" src="image/BG/almuhsin-telegram.png" alt="">
                             <div class="SH_icone_name">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm121.8 169.9l-40.7 191.8c-3 13.6-11.1 16.9-22.4 10.5l-62-45.7-29.9 28.8c-3.3 3.3-6.1 6.1-12.5 6.1l4.4-63.1 114.9-103.8c5-4.4-1.1-6.9-7.7-2.5l-142 89.4-61.2-19.1c-13.3-4.2-13.6-13.3 2.8-19.7l239.1-92.2c11.1-4 20.8 2.7 17.2 19.5z"/></svg>                               
                                 <span>تلغرام</span>
@@ -97,7 +97,7 @@
                         <img class="SH-top-img" src="image/ship.png" >
                         <h3>برنامج سفينة المرح</h3>
                         <div class="SH_image_container">
-                            <img class="SH_image" src="image/BG/re-facebook.png" alt="">
+                            <img class="SH_image" src="image/BG/facebook-02.png" alt="">
                             <div class="SH_icone_name">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"/></svg>                               
                                 <span>فيسبوك</span>
@@ -150,7 +150,7 @@
                         <img class="SH-top-img" src="image/iahs-green.png" >
                         <h3>مركز المحسن لثقافة الاطفال</h3>
                         <div class="SH_image_container">
-                            <img class="SH_image" src="image/BG/re-instagram.png" alt="">
+                            <img class="SH_image" src="image/BG/instagram-02.png" alt="">
                             <div class="SH_icone_name">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) --><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>                             
                                 <span>انستغرام</span>
